254dfc0470daa34817f4bf70d5ec4591f397ef88
Olof Johansson <olofj@tenstorrent.com>
2025-04-30 15:41:10 -0700

    hugepages-setup.sh: Fix shellcheck warnings

--- hugepages-setup/hugepages-setup.sh
+++ hugepages-setup/hugepages-setup.sh
@@ -6,6 +6,8 @@
 # For every Wormhole we allocate a full 4x1G pages, maximize the
 # aperture between device and host.
 
+set -eo pipefail
+
 TT_VID=1e52
 GS_PID=faca
 WH_PID=401e
@@ -14,11 +16,11 @@ BH_PID=b140
 # Two arguments: VIDPID to lspci, and multiplier per card found.
 # Returns: <node> <pages>, one per line per card
 get_node_pages() {
-   VIDPID=$1
-   MULT=$2
+   VIDPID="$1"
+   MULT="$2"
    # Need to do this since not all machines print NUMANode: for all devices, in that
    # case default to node 0.
-   lspci -d ${VIDPID} -vmm | awk "BEGIN {n=0} /NUMANode:/ {n=\$2} /^$/ {print n \" ${MULT}\"}"
+   lspci -d "${VIDPID}" -vmm | awk "BEGIN {n=0} /NUMANode:/ {n=\$2} /^$/ {print n \" ${MULT}\"}"
 }
 
 error_out() {
@@ -33,44 +35,44 @@ declare -A nodes
 file_path="/opt/tenstorrent/bin/hugepages-override.txt"
 if [[ -f "$file_path" ]]; then
     HP_OVERRIDE=$(<"$file_path")
-    TT_COUNT=$(lspci -d $TT_VID: | wc -l)
+    TT_COUNT=$(lspci -d "${TT_VID}": | wc -l)
     echo "hugepages override requested via hugepages-override.txt: $HP_OVERRIDE"
-    HP_COUNT=$(($HP_OVERRIDE / $TT_COUNT))
+    HP_COUNT=$((HP_OVERRIDE / TT_COUNT))
 
     while read -r index value ; do
         nodes[$index]=$((nodes[$index] + value))
     done < <(
-        get_node_pages "$TT_VID:$BH_PID" $HP_COUNT
-        get_node_pages "$TT_VID:$WH_PID" $HP_COUNT
-        get_node_pages "$TT_VID:$GS_PID" $HP_COUNT
+        get_node_pages "${TT_VID}:${BH_PID}" "$HP_COUNT"
+        get_node_pages "${TT_VID}:${WH_PID}" "$HP_COUNT"
+        get_node_pages "${TT_VID}:${GS_PID}" "$HP_COUNT"
     )
 else
     while read -r index value ; do
         nodes[$index]=$((nodes[$index] + value))
     done < <(
-        get_node_pages "$TT_VID:$BH_PID" 4
-        get_node_pages "$TT_VID:$WH_PID" 4
-        get_node_pages "$TT_VID:$GS_PID" 1
+        get_node_pages "${TT_VID}:${BH_PID}" 4
+        get_node_pages "${TT_VID}:${WH_PID}" 4
+        get_node_pages "${TT_VID}:${GS_PID}" 1
     )
 fi
 
 # Now, let's iterate over the nodes and configure the number of
 # hugepages.
-NODES="${!nodes[@]}"
+NODES=${!nodes[*]}
 
 # First, make sure we have the hugepages configured and available.
-for n in ${NODES} ; do
+for n in $NODES ; do
     NODEDIR="/sys/devices/system/node/node${n}"
     [ -d "${NODEDIR}" ] || error_out "Can't locate numa node directory at $NODEDIR. Check setup."
     HUGEPAGE_DIR="${NODEDIR}/hugepages/hugepages-1048576kB"
     [ -d "${HUGEPAGE_DIR}" ] || error_out "Can't locate 1GB hugepage settings at ${HUGEPAGE_DIR}. Check setup."
 
-    NR_HP="$(cat ${HUGEPAGE_DIR}/nr_hugepages)"
-    echo Node ${n} hugepages before: "${NR_HP}"
-    echo Node ${n} hugepages needed: "${nodes[$n]}"
-    echo "${nodes[$n]}" > ${HUGEPAGE_DIR}/nr_hugepages || error_out "Can't write to hugepages file at ${HUGEPAGE_DIR}/nr_hugepages"
-    NR_HP="$(cat ${HUGEPAGE_DIR}/nr_hugepages)"
-    echo Node ${n} hugepages after: "${NR_HP}"
+    NR_HP="$(cat "${HUGEPAGE_DIR}/nr_hugepages")"
+    echo "Node ${n} hugepages before: ${NR_HP}"
+    echo "Node ${n} hugepages needed: ${nodes[$n]}"
+    echo "${nodes[$n]}" > "${HUGEPAGE_DIR}/nr_hugepages" || error_out "Can't write to hugepages file at ${HUGEPAGE_DIR}/nr_hugepages"
+    NR_HP="$(cat "${HUGEPAGE_DIR}/nr_hugepages")"
+    echo "Node ${n} hugepages after: ${NR_HP}"
     if [ "${NR_HP}" != "${nodes[$n]}" ] ; then
         error_out "Failed to get requested ${nodes[$n]} hugepages, only got ${NR_HP}"
     fi
