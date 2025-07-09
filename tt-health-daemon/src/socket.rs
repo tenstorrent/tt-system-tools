814f9bc7411b286d2f4af93afe3cfa46f1060ffb
Petra Alexson <palexson@tenstorrent.com>
2025-07-09 17:10:32 -0400

    Expose some example tenstorrent device data on the socket
    Signed-off-by: Petra Alexson <palexson@tenstorrent.com>

--- tt-health-daemon/src/socket.rs
+++ tt-health-daemon/src/socket.rs
@@ -2,16 +2,34 @@ use std::os::unix::net::{UnixStream, UnixListener};
 use std::io::Write;
 use std::thread;
 use std::fs;
+use crate::device;
 
 fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
-    stream.write_all(b"hello world")?;
-    // TODO: expose something
+
+    let devices = device::get_tenstorrent_devices()?;
+
+    for dev in &devices {
+        let _ = stream.write_all(&format!("tt-asic-id: {}\n", dev.tt_asic_id.as_deref().unwrap_or("unknown")).into_bytes());
+        if let Some(counters) = &dev.pcie_perf_counters {
+            let _ = stream.write_all(&format!("mst_nonposted_wr_data_word_sent0: {}\n", counters.mst_nonposted_wr_data_word_sent0).into_bytes());
+            let _ = stream.write_all(&format!("mst_nonposted_wr_data_word_sent1: {}\n", counters.mst_nonposted_wr_data_word_sent1).into_bytes());
+            let _ = stream.write_all(&format!("mst_posted_wr_data_word_sent0: {}\n", counters.mst_posted_wr_data_word_sent0).into_bytes());
+            let _ = stream.write_all(&format!("mst_posted_wr_data_word_sent1: {}\n", counters.mst_posted_wr_data_word_sent1).into_bytes());
+            let _ = stream.write_all(&format!("mst_rd_data_word_received0: {}\n", counters.mst_rd_data_word_received0).into_bytes());
+            let _ = stream.write_all(&format!("mst_rd_data_word_received1: {}\n", counters.mst_rd_data_word_received1).into_bytes());
+            let _ = stream.write_all(&format!("slv_nonposted_wr_data_word_received0: {}\n", counters.slv_nonposted_wr_data_word_received0).into_bytes());
+            let _ = stream.write_all(&format!("slv_nonposted_wr_data_word_received1: {}\n", counters.slv_nonposted_wr_data_word_received1).into_bytes());
+            let _ = stream.write_all(&format!("slv_posted_wr_data_word_received0: {}\n", counters.slv_posted_wr_data_word_received0).into_bytes());
+            let _ = stream.write_all(&format!("slv_posted_wr_data_word_received1: {}\n", counters.slv_posted_wr_data_word_received1).into_bytes());
+            let _ = stream.write_all(&format!("slv_rd_data_word_sent0: {}\n", counters.slv_rd_data_word_sent0).into_bytes());
+            let _ = stream.write_all(&format!("slv_rd_data_word_sent1: {}\n", counters.slv_rd_data_word_sent1).into_bytes());
+        }
+    }
+
     Ok(())
 }
 
-pub fn start_unix_listener() -> std::io::Result<()> {
-    let socket_path = "sock";
-
+pub fn start_unix_listener(socket_path: &str) -> std::io::Result<()> {
     // if socket_path already exists, clean it up
     if fs::metadata(socket_path).is_ok() {
         fs::remove_file(socket_path)?;
@@ -24,11 +42,11 @@ pub fn start_unix_listener() -> std::io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                /* connection succeeded */
+                // Connection succeeded 
                 thread::spawn(|| handle_client(stream));
             }
             Err(_) => {
-                /* connection failed */
+                // Connection failed
                 break;
             }
         }
