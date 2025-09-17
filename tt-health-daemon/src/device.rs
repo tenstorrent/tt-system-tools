45136bff1abf5f72965973a75ca4188cbfb12e57
Petra Alexson <palexson@tenstorrent.com>
2025-09-17 14:46:37 -0400

    Initialize TTDevice structs as empty
    Add .new() methods for TTDevice, PciePerfCounters, and Telemetry to create empty
    TTDevice structs. Instead of initializing with all the fields populated, use
    update methods to update the info. This is so later when there is e.g. rollover
    logic on certain values comparisons between previous and new field values can
    be handled more easily.
    
    Also derive Debug on all structs to simplify the data dump code in socket.rs
    
    Signed-off-by: Petra Alexson <palexson@tenstorrent.com>

--- tt-health-daemon/src/device.rs
+++ tt-health-daemon/src/device.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
-use std::time::SystemTime;
 use std::fs;
 use std::io;
-
-pub const TT_SYS_DIR: &str = "/sys/class/tenstorrent";
+use chrono::{DateTime, Utc};
 
 // Tenstorrent device attribute struct
 // Keeping all attributes as Strings for now
+#[derive(Debug)]
 pub struct TTDevice {
-    pub timestamp: Option<SystemTime>,
+    pub time: DateTime<Utc>,
+    pub device_path: String,
     pub tt_aiclk: Option<String>,
     pub tt_arcclk: Option<String>,
     pub tt_asic_id: Option<String>,
@@ -17,105 +17,150 @@ pub struct TTDevice {
     pub tt_fw_bundle_ver: Option<String>,
     pub tt_m3app_fw_ver: Option<String>,
     pub tt_serial: Option<String>,
-    pub pcie_perf_counters: Option<PciePerfCounters>,
-    pub telemetry: Option<Telemetry>,
+    pub pcie_perf_counters: PciePerfCounters,
+    pub telemetry: Telemetry,
 }
 
+#[derive(Debug)]
 pub struct PciePerfCounters {
-    pub mst_nonposted_wr_data_word_sent0: u32,
-    pub mst_nonposted_wr_data_word_sent1: u32,
-    pub mst_posted_wr_data_word_sent0: u32,
-    pub mst_posted_wr_data_word_sent1: u32,
-    pub mst_rd_data_word_received0: u32,
-    pub mst_rd_data_word_received1: u32,
-    pub slv_nonposted_wr_data_word_received0: u32,
-    pub slv_nonposted_wr_data_word_received1: u32,
-    pub slv_posted_wr_data_word_received0: u32,
-    pub slv_posted_wr_data_word_received1: u32,
-    pub slv_rd_data_word_sent0: u32,
-    pub slv_rd_data_word_sent1: u32,
+    pub mst_nonposted_wr_data_word_sent0: Option<u32>,
+    pub mst_nonposted_wr_data_word_sent1: Option<u32>,
+    pub mst_posted_wr_data_word_sent0: Option<u32>,
+    pub mst_posted_wr_data_word_sent1: Option<u32>,
+    pub mst_rd_data_word_received0: Option<u32>,
+    pub mst_rd_data_word_received1: Option<u32>,
+    pub slv_nonposted_wr_data_word_received0: Option<u32>,
+    pub slv_nonposted_wr_data_word_received1: Option<u32>,
+    pub slv_posted_wr_data_word_received0: Option<u32>,
+    pub slv_posted_wr_data_word_received1: Option<u32>,
+    pub slv_rd_data_word_sent0: Option<u32>,
+    pub slv_rd_data_word_sent1: Option<u32>,
 }
 
+#[derive(Debug)]
 pub struct Telemetry {
-    pub current: u32,
-    pub power: u32,
-    pub asic_temp: u32,
-    pub vcore: u32,
-    pub fan_rpm: u32,
+    pub current: Option<u32>,
+    pub power: Option<u32>,
+    pub asic_temp: Option<u32>,
+    pub vcore: Option<u32>,
+    pub fan_rpm: Option<u32>,
 }
 
-fn sysfs_read_to_u32(path: &Path) -> io::Result<u32> {
-    let content = fs::read_to_string(&path)?;
-    let content_trim = &content.trim();
-    u32::from_str_radix(content_trim, 10).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+fn sysfs_read_to_u32(path: &Path) -> Option<u32> {
+    let content = fs::read_to_string(&path).ok()?;
+    let content_trim = content.trim();
+    u32::from_str_radix(content_trim, 10).ok()
 }
 
-fn sysfs_read_to_string(path: &Path) -> io::Result<String> {
-    let content = fs::read_to_string(&path)?;
-    Ok(content.trim().to_string())
+fn sysfs_read_to_string(path: &Path) -> Option<String> {
+    let content = fs::read_to_string(&path).ok()?;
+    Some(content.trim().to_string()) // Trim \n from the end
 }
 
 impl TTDevice {
-    fn from_dir(path: &Path) -> io::Result<Self> {
-        Ok(Self {
-            timestamp: Some(SystemTime::now()),
-            tt_aiclk: sysfs_read_to_string(&path.join("tt_aiclk")).ok(),
-            tt_arcclk: sysfs_read_to_string(&path.join("tt_arcclk")).ok(),
-            tt_asic_id: sysfs_read_to_string(&path.join("tt_asic_id")).ok(),
-            tt_axiclk: sysfs_read_to_string(&path.join("tt_axiclk")).ok(),
-            tt_card_type: sysfs_read_to_string(&path.join("tt_card_type")).ok(),
-            tt_fw_bundle_ver: sysfs_read_to_string(&path.join("tt_fw_bundle_ver")).ok(),
-            tt_m3app_fw_ver: sysfs_read_to_string(&path.join("tt_m3app_fw_ver")).ok(),
-            tt_serial: sysfs_read_to_string(&path.join("tt_serial")).ok(),
-            pcie_perf_counters: PciePerfCounters::from_dir(&path.join("pcie_perf_counters")).ok(),
-            telemetry: Telemetry::from_dir(path).ok(),
-        })
+    pub fn new(path: &Path) -> Self {
+        Self {
+            device_path: path.to_string_lossy().to_string(),
+            time: Utc::now(),
+            tt_aiclk: None,
+            tt_arcclk: None,
+            tt_asic_id: None,
+            tt_axiclk: None,
+            tt_card_type: None,
+            tt_fw_bundle_ver: None,
+            tt_m3app_fw_ver: None,
+            tt_serial: None,
+            pcie_perf_counters: PciePerfCounters::new(),
+            telemetry: Telemetry::new(),
+        }
+    }
+
+    pub fn update(&mut self) -> io::Result<()> {
+        let path = Path::new(&self.device_path);
+        self.time = Utc::now();
+        self.tt_aiclk = sysfs_read_to_string(&path.join("tt_aiclk"));
+        self.tt_arcclk = sysfs_read_to_string(&path.join("tt_arcclk"));
+        self.tt_asic_id = sysfs_read_to_string(&path.join("tt_asic_id"));
+        self.tt_axiclk = sysfs_read_to_string(&path.join("tt_axiclk"));
+        self.tt_card_type = sysfs_read_to_string(&path.join("tt_card_type"));
+        self.tt_fw_bundle_ver = sysfs_read_to_string(&path.join("tt_fw_bundle_ver"));
+        self.tt_m3app_fw_ver = sysfs_read_to_string(&path.join("tt_m3app_fw_ver"));
+        self.tt_serial = sysfs_read_to_string(&path.join("tt_serial"));
+        self.pcie_perf_counters.update_from_dir(&path.join("pcie_perf_counters"));
+        self.telemetry.update_from_dir(&path.join("device/hwmon/hwmon2/")); // TODO: which paths to inspect
+        Ok(())
     }
 }
 
 impl PciePerfCounters {
-    fn from_dir(path: &Path) -> io::Result<Self> {
-        let instance = Self {
-            mst_nonposted_wr_data_word_sent0: sysfs_read_to_u32(&path.join("mst_nonposted_wr_data_word_sent0"))?,
-            mst_nonposted_wr_data_word_sent1: sysfs_read_to_u32(&path.join("mst_nonposted_wr_data_word_sent1"))?,
-            mst_posted_wr_data_word_sent0: sysfs_read_to_u32(&path.join("mst_posted_wr_data_word_sent0"))?,
-            mst_posted_wr_data_word_sent1: sysfs_read_to_u32(&path.join("mst_posted_wr_data_word_sent1"))?,
-            mst_rd_data_word_received0: sysfs_read_to_u32(&path.join("mst_rd_data_word_received0"))?,
-            mst_rd_data_word_received1: sysfs_read_to_u32(&path.join("mst_rd_data_word_received1"))?,
-            slv_nonposted_wr_data_word_received0: sysfs_read_to_u32(&path.join("slv_nonposted_wr_data_word_received0"))?,
-            slv_nonposted_wr_data_word_received1: sysfs_read_to_u32(&path.join("slv_nonposted_wr_data_word_received1"))?,
-            slv_posted_wr_data_word_received0: sysfs_read_to_u32(&path.join("slv_posted_wr_data_word_received0"))?,
-            slv_posted_wr_data_word_received1: sysfs_read_to_u32(&path.join("slv_posted_wr_data_word_received1"))?,
-            slv_rd_data_word_sent0: sysfs_read_to_u32(&path.join("slv_rd_data_word_sent0"))?,
-            slv_rd_data_word_sent1: sysfs_read_to_u32(&path.join("slv_rd_data_word_sent1"))?
-        };
+    pub fn new() -> Self {
+        Self {
+            mst_nonposted_wr_data_word_sent0: None,
+            mst_nonposted_wr_data_word_sent1: None,
+            mst_posted_wr_data_word_sent0: None,
+            mst_posted_wr_data_word_sent1: None,
+            mst_rd_data_word_received0: None,
+            mst_rd_data_word_received1: None,
+            slv_nonposted_wr_data_word_received0: None,
+            slv_nonposted_wr_data_word_received1: None,
+            slv_posted_wr_data_word_received0: None,
+            slv_posted_wr_data_word_received1: None,
+            slv_rd_data_word_sent0: None,
+            slv_rd_data_word_sent1: None,
+        }
+    }
 
-        Ok(instance)
+    pub fn update_from_dir(&mut self, path: &Path) {
+        self.mst_nonposted_wr_data_word_sent0 = sysfs_read_to_u32(&path.join("mst_nonposted_wr_data_word_sent0"));
+        self.mst_nonposted_wr_data_word_sent1 = sysfs_read_to_u32(&path.join("mst_nonposted_wr_data_word_sent1"));
+        self.mst_posted_wr_data_word_sent0 = sysfs_read_to_u32(&path.join("mst_posted_wr_data_word_sent0"));
+        self.mst_posted_wr_data_word_sent1 = sysfs_read_to_u32(&path.join("mst_posted_wr_data_word_sent1"));
+        self.mst_rd_data_word_received0 = sysfs_read_to_u32(&path.join("mst_rd_data_word_received0"));
+        self.mst_rd_data_word_received1 = sysfs_read_to_u32(&path.join("mst_rd_data_word_received1"));
+        self.slv_nonposted_wr_data_word_received0 = sysfs_read_to_u32(&path.join("slv_nonposted_wr_data_word_received0"));
+        self.slv_nonposted_wr_data_word_received1 = sysfs_read_to_u32(&path.join("slv_nonposted_wr_data_word_received1"));
+        self.slv_posted_wr_data_word_received0 = sysfs_read_to_u32(&path.join("slv_posted_wr_data_word_received0"));
+        self.slv_posted_wr_data_word_received1 = sysfs_read_to_u32(&path.join("slv_posted_wr_data_word_received1"));
+        self.slv_rd_data_word_sent0 = sysfs_read_to_u32(&path.join("slv_rd_data_word_sent0"));
+        self.slv_rd_data_word_sent1 = sysfs_read_to_u32(&path.join("slv_rd_data_word_sent1"));
     }
 }
 
 impl Telemetry {
-    fn from_dir(path: &Path) -> io::Result<Self> {
-        const HWMON_PATH: &str = "device/hwmon/hwmon2/"; // TODO: is it always hwmon2
-        let base_path = path.join(HWMON_PATH);
-        let instance = Self {
-            current: sysfs_read_to_u32(&base_path.join("curr1_input"))?,
-            power: sysfs_read_to_u32(&base_path.join("power1_input"))?,
-            asic_temp: sysfs_read_to_u32(&base_path.join("temp1_input"))?,
-            vcore: sysfs_read_to_u32(&base_path.join("in0_input"))?,
-            fan_rpm: sysfs_read_to_u32(&base_path.join("fan1_input"))?,
-        };
+    pub fn new() -> Self {
+        Self {
+            current: None,
+            power: None,
+            asic_temp: None,
+            vcore: None,
+            fan_rpm: None,
+        }
+    }
 
-        Ok(instance)
+    pub fn update_from_dir(&mut self, path: &Path) {
+        self.current = sysfs_read_to_u32(&path.join("curr1_input"));
+        self.power = sysfs_read_to_u32(&path.join("power1_input"));
+        self.asic_temp = sysfs_read_to_u32(&path.join("temp1_input"));
+        self.vcore = sysfs_read_to_u32(&path.join("in0_input"));
+        self.fan_rpm = sysfs_read_to_u32(&path.join("fan1_input"));
     }
 }
 
-pub fn get_tenstorrent_devices() -> io::Result<Vec<TTDevice>>  {
+pub fn get_tenstorrent_devices() -> io::Result<Vec<TTDevice>> {
     let mut devices = Vec::new();
 
-    for entry in fs::read_dir(TT_SYS_DIR)? {
-        devices.push(TTDevice::from_dir(&entry?.path())?);
+    for entry in fs::read_dir("/sys/class/tenstorrent")? {
+        let path = entry?.path();
+        let device = TTDevice::new(&path);
+        devices.push(device);
     }
     
     Ok(devices)
+}
+
+pub fn update_tenstorrent_devices(devices: &mut [TTDevice]) -> io::Result<()> {
+    for device in devices {
+        device.update()?;
+    }
+    Ok(())
 }
--- tt-health-daemon/src/socket.rs
+++ tt-health-daemon/src/socket.rs
@@ -7,39 +7,11 @@ use crate::device;
 // TODO: Turn this into a metrics exporter using conventional formats
 
 fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
+    let mut devices = device::get_tenstorrent_devices()?;
+    device::update_tenstorrent_devices(&mut devices)?;
 
-    let devices = device::get_tenstorrent_devices()?;
-
-    for dev in &devices {
-        let _ = stream.write_all(&format!("tt_asic_id: {}\n", dev.tt_asic_id.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_aiclk: {}\n", dev.tt_aiclk.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_arcclk: {}\n", dev.tt_arcclk.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_axiclk: {}\n", dev.tt_axiclk.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_card_type: {}\n", dev.tt_card_type.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_fw_bundle_ver: {}\n", dev.tt_fw_bundle_ver.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_m3app_fw_ver: {}\n", dev.tt_m3app_fw_ver.as_deref().unwrap_or("unknown")).into_bytes());
-        let _ = stream.write_all(&format!("tt_serial: {}\n", dev.tt_serial.as_deref().unwrap_or("unknown")).into_bytes());
-        if let Some(counters) = &dev.pcie_perf_counters {
-            let _ = stream.write_all(&format!("mst_nonposted_wr_data_word_sent0: {}\n", counters.mst_nonposted_wr_data_word_sent0).into_bytes());
-            let _ = stream.write_all(&format!("mst_nonposted_wr_data_word_sent1: {}\n", counters.mst_nonposted_wr_data_word_sent1).into_bytes());
-            let _ = stream.write_all(&format!("mst_posted_wr_data_word_sent0: {}\n", counters.mst_posted_wr_data_word_sent0).into_bytes());
-            let _ = stream.write_all(&format!("mst_posted_wr_data_word_sent1: {}\n", counters.mst_posted_wr_data_word_sent1).into_bytes());
-            let _ = stream.write_all(&format!("mst_rd_data_word_received0: {}\n", counters.mst_rd_data_word_received0).into_bytes());
-            let _ = stream.write_all(&format!("mst_rd_data_word_received1: {}\n", counters.mst_rd_data_word_received1).into_bytes());
-            let _ = stream.write_all(&format!("slv_nonposted_wr_data_word_received0: {}\n", counters.slv_nonposted_wr_data_word_received0).into_bytes());
-            let _ = stream.write_all(&format!("slv_nonposted_wr_data_word_received1: {}\n", counters.slv_nonposted_wr_data_word_received1).into_bytes());
-            let _ = stream.write_all(&format!("slv_posted_wr_data_word_received0: {}\n", counters.slv_posted_wr_data_word_received0).into_bytes());
-            let _ = stream.write_all(&format!("slv_posted_wr_data_word_received1: {}\n", counters.slv_posted_wr_data_word_received1).into_bytes());
-            let _ = stream.write_all(&format!("slv_rd_data_word_sent0: {}\n", counters.slv_rd_data_word_sent0).into_bytes());
-            let _ = stream.write_all(&format!("slv_rd_data_word_sent1: {}\n", counters.slv_rd_data_word_sent1).into_bytes());
-        }
-        if let Some(telemetry) = &dev.telemetry {
-            let _ = stream.write_all(&format!("current: {}\n", telemetry.current).into_bytes());
-            let _ = stream.write_all(&format!("power: {}\n", telemetry.power).into_bytes());
-            let _ = stream.write_all(&format!("asic_temp: {}\n", telemetry.asic_temp).into_bytes());
-            let _ = stream.write_all(&format!("vcore: {}\n", telemetry.vcore).into_bytes());
-            let _ = stream.write_all(&format!("fan_rpm: {}\n", telemetry.fan_rpm).into_bytes());
-        }
+    for dev in devices {
+        let _ = stream.write_all(&format!("{:?}", dev).into_bytes());
     }
 
     Ok(())
